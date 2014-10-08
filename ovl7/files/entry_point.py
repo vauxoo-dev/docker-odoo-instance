95e2ba1315ee1a1c37485aa940af984038aa8f84
Tulio Ruiz <tulio@vauxoo.com>
2014-10-08 18:13:17 -0500

    [ADD] modified entry point so it uses env variables for connecting to db

--- ovl7/files/entry_point.py
+++ ovl7/files/entry_point.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-from os import listdir, stat, chown, path
-from subprocess import Popen, call
+from os import stat, path, getenv
+from subprocess import call
 from shutil import copy2
 import pwd
+import fileinput
 
 filestore_path = '/home/odoo/instance/odoo/openerp/filestore'
 configfile_path = '/home/odoo/instance/config/odoo.conf'
@@ -15,6 +17,20 @@ if owner != "odoo":
 
 if not path.isfile(configfile_path):
     copy2("/external_files/odoo.conf", configfile_path)
+
+if getenv('DB_SERVER'):
+    for line in fileinput.input(configfile_path, inplace=True):
+        if line.startswith('db_host'):
+            print('db_host = %s' % getenv('DB_SERVER'))
+        else:
+            print(line.replace('\n', ''))
+
+if getenv('DB_PORT'):
+    for line in fileinput.input(configfile_path, inplace=True):
+        if line.startswith('db_port'):
+            print('db_port = %s' % getenv('DB_PORT'))
+        else:
+            print(line.replace('\n', ''))
 
 st = stat(configfile_path)
 owner = pwd.getpwuid(st.st_uid).pw_name
