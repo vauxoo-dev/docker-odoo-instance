f7915500da1c3160076b777a4d5578f3c32f3ba2
Moises Lopez <moylop260@vauxoo.com>
2015-09-26 19:00:25 -0500

    [REF] runbot90: Add patches

--- runbot90/files/entrypoint.sh
+++ runbot90/files/entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 chown -R runbot:runbot ${EXTRA_ADDONS}/odoo-extra/runbot/static
 chown -R runbot:runbot /home/runbot/.local/share/Odoo/filestore
 chown -R runbot:runbot /var/log/supervisor
-find /home/runbot/instance/extra_addons/odoo-extra/runbot -name res_config_view.xml -exec sed -i 's/base.menu_config/base.menu_administration/g' {} \;
 source /home/runbot/.db_source
 
 /usr/bin/supervisord
