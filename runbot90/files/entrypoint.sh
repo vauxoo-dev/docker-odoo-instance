4f24d958980b5ff3fa30a2181031d556770b1db1
Moises Lopez <moylop260@vauxoo.com>
2015-09-28 17:46:55 -0500

    [REF] runbot90: Add docker to supervisor and volume

--- runbot90/files/entrypoint.sh
+++ runbot90/files/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 chown -R runbot:runbot ${EXTRA_ADDONS}/odoo-extra/runbot/static
 chown -R runbot:runbot /home/runbot/.local/share/Odoo/filestore
 chown -R runbot:runbot /var/log/supervisor
+chown -R runbot:runbot /var/lib/docker
 source /home/runbot/.db_source
 
 /usr/bin/supervisord
