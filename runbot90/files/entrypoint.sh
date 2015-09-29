f8fab872e45ee7af319831dbc42ba6ee9858beda
Moises Lopez <moylop260@vauxoo.com>
2015-09-29 05:32:26 -0400

    [REF] runbot90: Add checks for folder permissions

--- runbot90/files/entrypoint.sh
+++ runbot90/files/entrypoint.sh
@@ -6,6 +6,7 @@ chown -R runbot:runbot ${EXTRA_ADDONS}/odoo-extra/runbot/static
 chown -R runbot:runbot /home/runbot/.local/share/Odoo/filestore
 chown -R runbot:runbot /var/log/supervisor
 chown -R runbot:runbot /var/lib/docker
-source /home/runbot/.db_source
+chmod ugo+rwx /tmp
 
+source /home/runbot/.db_source
 /usr/bin/supervisord
